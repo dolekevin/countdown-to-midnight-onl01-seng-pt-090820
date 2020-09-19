27ccee16342d8d203bcfbf56d0cad771d03b9bd5
Kevin Dole <dole.kevin@gmail.com>
2020-09-19 22:46:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(number)
   while countdown(10) > 0 
-  puts "countdown (#{number}) SECOND(S)!"
+  puts "countdown #{number} SECOND(S)!"
   #{number} -= 1
   end
   return "HAPPY NEW YEAR!"
