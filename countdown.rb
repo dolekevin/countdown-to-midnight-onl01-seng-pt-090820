088f193ebe6e1f349f6d89b9216bffc9a1503fd7
Kevin Dole <dole.kevin@gmail.com>
2020-09-19 23:12:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,11 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  sleep(1)
+  number -= 1
+  end
+  "HAPPY NEW YEAR!"
   
 end
